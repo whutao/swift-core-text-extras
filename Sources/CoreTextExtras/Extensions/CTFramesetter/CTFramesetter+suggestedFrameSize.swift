4e2338844f6fbcab2e00f25ebd392a2374ab5a93
whutao <whutao.dev@gmail.com>
2025-05-15 17:53:17 +0300

    Add CTFramesetter suggestedFrameSize convenience method.

--- Sources/CoreTextExtras/Extensions/CTFramesetter/CTFramesetter+suggestedFrameSize.swift
+++ Sources/CoreTextExtras/Extensions/CTFramesetter/CTFramesetter+suggestedFrameSize.swift
@@ -15,6 +15,29 @@ extension CTFramesetter {
     ///   - constraints: The `CGSize` specifying the width and height constraints for the frame.
     ///   - attributes: A dictionary of frame attributes passed to `CTFramesetterSuggestFrameSizeWithConstraints`.
     ///
+    /// - Returns: The suggested frame size that fits within the given constraints.
+    @inlinable
+    public func suggestedFrameSize(
+        for range: NSRange = NSRange(),
+        constrainedBy constraints: CGSize,
+        attributes: [CFString: Any] = [:]
+    ) -> CGSize {
+        let (size, _) = suggestedFrameSize(for: range, constrainedBy: constraints, attributes: attributes)
+        return size
+    }
+    
+    /// Suggests a frame size for the given constraints.
+    ///
+    /// This method calculates and returns the size of the frame that best fits within the given constraints.
+    ///
+    /// - Parameters:
+    ///   - range: The string range to which the frame size applies.
+    ///     The string range is a range over the string used to create the framesetter.
+    ///     If the length portion of the range is set to 0, then the framesetter continues
+    ///     to add lines until it runs out of text or space.
+    ///   - constraints: The `CGSize` specifying the width and height constraints for the frame.
+    ///   - attributes: A dictionary of frame attributes passed to `CTFramesetterSuggestFrameSizeWithConstraints`.
+    ///
     /// - Returns: A tuple containing:
     ///   - size: A `CGSize` representing the suggested frame size that fits within the given constraints.
     ///   - fitRange: An `NSRange` indicating the portion of the string that fits within the returned size.
